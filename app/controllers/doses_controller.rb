916f8aa959f5f1822d218fc2a9b25c939ec1350c
Lena Schlage <lenaleasing@web.de>
2020-08-20 01:01:06 +0200

    commit

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,30 +1,25 @@
 class DosesController < ApplicationController  
-    def new
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose = Dose.new
-  end
-
-  def create
-    @dose = Dose.new(dose_params)
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose.cocktail = @cocktail
-    if @dose.save
-      redirect_to cocktail_path(@cocktail)
-    else
-      render :new
-    end
-  end
-
-  def destroy
-    @dose = Dose.find(params[:id])
-    @cocktail = Cocktail.find(@dose.cocktail_id)
-    @dose.destroy
-    redirect_to cocktail_path(@cocktail)
-  end
-
-  private
-
-  def dose_params
-    params.require(:dose).permit(:description, :ingredient_id, :dose_id)
-  end
+    def create
+        @cocktail = Cocktail.find(params[:cocktail_id])
+        @dose = Dose.new(dose_params)
+        @dose.cocktail = @cocktail
+        if @dose.save
+          redirect_to cocktail_path(@cocktail)
+        else
+          @review = Review.new
+          render "cocktails/show"
+        end
+      end
+    
+      def destroy
+        @dose = Dose.find(params[:id])
+        @dose.destroy
+        redirect_to cocktail_path(@dose.cocktail)
+      end
+    
+      private
+    
+      def dose_params
+        params.require(:dose).permit(:description, :ingredient_id)
+      end
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,8 +1,5 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-  validates :description, presence: true, allow_blank: false
-  validates :cocktail, presence: true
-  validates :ingredient, presence: true
-  validates :cocktail_id, :uniqueness => { :scope => :ingredient_id }
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
